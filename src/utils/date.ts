b0ed0396703842010966f8589097d65ffa788a6d
Mathis Le Bonniec <mathislbonniec@gmail.com>
2022-02-06 20:38:46 +0100

    docs: add doctring to utils functions

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,14 @@
+/**
+ * Get the ISO String of the current date with a day shift
+ * @param shift Amount of day to shift the date
+ * @returns A string containing the date as ISO
+ * @example
+ * const date = getDateFormat(10);
+ * console.log(date);
+ */
 export function getDateFormat(shift: number): string {
-    const date = new Date();
+  const date = new Date();
 
-    date.setDate(date.getDate() + shift);
-    return date.toISOString().slice(0, 10);
+  date.setDate(date.getDate() + shift);
+  return date.toISOString().slice(0, 10);
 }
--- src/utils/get-schedule.ts
+++ src/utils/get-schedule.ts
@@ -1,6 +1,16 @@
 import { APIEvent } from 'types/event';
 import { Schedule } from 'types/schedule';
 
+/**
+ * Returns a interface with start and end date
+ * @remarks
+ * This function gives you the start and end dates from the Epitech API.
+ * @param request An APIEvent object
+ * @returns A structure containing two properties: start and end.
+ * @example
+ * const schedule = getSchedule(event);
+ * console.log(schedule.start, schedule.end);
+ */
 export function getSchedule(request: APIEvent): Schedule {
   const splitted = (request.rdv_group_registered || request.rdv_indiv_registered)?.split('|');
 
